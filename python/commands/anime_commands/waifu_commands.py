89fb97b81fec29bfc2ea44b4cd8b580b07ed0011
Cauã Oliveira <huskyyg0d@gmail.com>
2022-07-09 17:57:34 -0300

    added find_anime command

--- python/commands/anime_commands/waifu_commands.py
+++ python/commands/anime_commands/waifu_commands.py
@@ -1,5 +1,6 @@
 import discord
 import requests
+import urllib.parse
 from discord import app_commands
 from discord.ext import commands
 from discord.app_commands import Choice
@@ -73,6 +74,52 @@ class WaifuCommands(commands.Cog):
         await interaction.response.send_message(embed=embed, view=view)
         out = await interaction.original_message()
         view.response = out
+
+
+
+    @app_commands.command(name='find-anime', description="Find anime by the image that you use!")
+    async def find_anime(self, interaction: discord.Interaction, image: discord.Attachment):
+        image = image.url
+        r = requests.get("https://api.trace.moe/search?url={}".format(urllib.parse.quote_plus(image))).json()
+        image2 = r['result'][0]['image']
+        episode = r['result'][0]['episode']
+        video = r['result'][0]['video']
+        id = r['result'][0]['anilist']
+        anilist = f'https://anilist.co/anime/{str(id)}/'
+        myanimelist = f'https://myanimelist.net/anime/{id}'
+        similarity = r['result'][0]['similarity'] * 100
+        similarity = "{:.2f}".format(similarity)
+        query = '''
+            query ($id: Int) { # Define which variables will be used in the query (id)
+              Media (id: $id, type: ANIME) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
+                id
+                title {
+                  romaji
+                  english
+                  native
+                }
+              }
+            }
+            '''
+
+        # Define our query variables and values that will be used in the query request
+        variables = {
+            'id': id
+        }
+        url = 'https://graphql.anilist.co'
+        # Make the HTTP Api request
+        response = requests.post(url, json={'query': query, 'variables': variables})
+        title = response.json()['data']['Media']['title']['english']
+
+
+        # Embed
+
+        embed = discord.Embed(description=f"Title: **{title}** \nSimilarity: **{similarity}%** \nEpisode: **{episode}**  \nShort Clip: [**Link**]({video})  \nAnilist: [**Link**]({anilist})  \nMyanimelist: [**Link**]({myanimelist})",color=discord.Colour.blue())
+        embed.set_author(name="That's what i've found!",icon_url="https://cdn.discordapp.com/avatars/989409439956213830/b9336d36eb09936ca2405830600c1bc3.png?size=1024")
+        embed.set_image(url=image2)
+        embed.set_footer(icon_url=interaction.user.avatar.url, text=f'Requested by: {interaction.user.name}')
+
+        await interaction.response.send_message(embed=embed)
 
 
 
